d4b593b27eb2b9126a579dd68eb4df181d09f1d1
Carlos <bowyercarlos@gmail.com>
2023-11-03 14:13:32 -0400

    final

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "Let me tell you a bit about who I am...";
 document.querySelector("body").appendChild(h2);
 
+const pictures = document.getElementsByClassName ("images") 
+console.log(pictures)
+
