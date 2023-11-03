9b4464780753e0cb5f5ea92c30d9179b33b1484e
Carlos <bowyercarlos@gmail.com>
2023-11-03 12:18:28 -0400

    Initialize commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Let me tell you a bit about who I am...";
 document.querySelector("body").appendChild(h2);
 
